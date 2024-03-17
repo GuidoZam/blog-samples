8779d4eaed54785e86009ee82a703e4d65e351c1
guidozam <guido@piasys.it>
2024-03-17 19:21:13 +0100

    Added mgt-picker-spfx

---
+++ MGT/mgt-picker-spfx/src/webparts/mgtPicker/loc/mystrings.d.ts
@@ -0,0 +1,20 @@
+declare interface IMgtPickerWebPartStrings {
+	PropertyPaneDescription: string;
+	BasicGroupName: string;
+	DescriptionFieldLabel: string;
+	SelectTaskList: string;
+	SelectTask: string;
+	SelectedTask: string;
+	Status: string;
+	Importance: string;
+	Tasks: string;
+	Users: string;
+	SelectUser: string
+  Events: string;
+  SelectEvent: string;
+}
+
+declare module 'MgtPickerWebPartStrings' {
+  const strings: IMgtPickerWebPartStrings;
+  export = strings;
+}
