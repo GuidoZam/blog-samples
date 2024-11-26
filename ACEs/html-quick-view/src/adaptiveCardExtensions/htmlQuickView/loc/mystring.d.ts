1b617aca771142cb43311c61728aced8cdc81779
guidozam <guido@piasys.it>
2024-11-26 14:11:01 +0100

    Added ACE with HTML Quick View

---
+++ ACEs/html-quick-view/src/adaptiveCardExtensions/htmlQuickView/loc/mystring.d.ts
@@ -0,0 +1,16 @@
+declare interface IHtmlQuickViewAdaptiveCardExtensionStrings {
+  PropertyPaneDescription: string;
+  TitleFieldLabel: string;
+  Title: string;
+  SubTitle: string;
+  PrimaryText: string;
+  Description: string;
+  QuickViewButton: string;
+  HtmlTitle: string;
+  HtmlDescription: string;
+}
+
+declare module 'HtmlQuickViewAdaptiveCardExtensionStrings' {
+  const strings: IHtmlQuickViewAdaptiveCardExtensionStrings;
+  export = strings;
+}
