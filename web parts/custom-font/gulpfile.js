7d489180025d4358cd1b3e1c28b53dd98eca8898
guidozam <guido@piasys.it>
2025-04-02 09:01:50 +0200

    Changed font to Oxygen and updated webpack configuration

--- web parts/custom-font/gulpfile.js
+++ web parts/custom-font/gulpfile.js
@@ -6,14 +6,9 @@ build.addSuppression(`Warning - [sass] The local CSS class 'ms-Grid' is not came
 
 // Font loader configuration
 const fontLoaderConfig = {
-  test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
-  use: [{
-    loader: 'file-loader',
-    options: {
-      name: '[name].[ext]',
-      outputPath: 'fonts/'
-    }
-  }]
+	test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+	type: "asset/resource",
+	dependency: { not: ["url"] },
 };
 
 // Merge custom font loader
