6599d2fc6b709e465d63420518f449b214274f35
Guido Zambarda <gudkaronte+lavoro@gmail.com>
2026-02-15 22:46:09 +0100

    Initial commit for personal-settings web part sample.

---
+++ web parts/personal-settings/src/webparts/personalSettings/loc/en-us.js
@@ -0,0 +1,11 @@
+define([], function() {
+  return {
+		Title: 'Personal Settings',
+		SaveConfig: 'Save Config',
+		LoadConfig: 'Load Config',
+		ConfigLabel: 'Configuration',
+		ConfigContentPlaceholder:
+			'Enter your content here. Use "Load Config" to load your settings, or type any content to save as a new file.',
+    Loading: 'Loading...'
+	};
+});
